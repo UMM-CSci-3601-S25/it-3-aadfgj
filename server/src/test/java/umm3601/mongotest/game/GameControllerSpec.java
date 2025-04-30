4f4981dae861e89b55f9e99d36ec39ced05908fe
George Savvas <georg.savvas@gmail.com>
2025-04-29 22:10:56 -0500

    removed unused import
    co-authored-by: Fiona
    <husse412@morris.umn.edu>

--- server/src/test/java/umm3601/mongotest/game/GameControllerSpec.java
+++ server/src/test/java/umm3601/mongotest/game/GameControllerSpec.java
@@ -2,7 +2,7 @@ package umm3601.mongotest.game;
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 // import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+// import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.Mockito.mock;
